759bea470832778e5e865ee13d100148781a8112
Maxim <max77648@gmail.com>
2020-09-09 20:27:43 +0300

    Оптимизироваел тренарный оператор

--- FirstLabaCMK/BinaryPow.cs
+++ FirstLabaCMK/BinaryPow.cs
@@ -11,7 +11,8 @@ namespace FirstLabaCMK
             int result = 1;
             for (int i = 0; i < numberInBinary.Count; i++)
             {
-                result = (numberInBinary[i] == 0) ?  Cripto.GetModulo(result * result, mod) : Cripto.GetModulo(Cripto.GetModulo(result * result, mod) * number, mod);
+                int result2 = Cripto.GetModulo(result * result, mod);
+                result = (numberInBinary[i] == 0) ? result2 : Cripto.GetModulo(result2 * number, mod);
             }
             return result;
         }
