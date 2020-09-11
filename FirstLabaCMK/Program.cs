33a8ac16420f2f4dc31eab88343e201480cf45eb
Maxim <max77648@gmail.com>
2020-09-11 11:31:18 +0300

    Начал делать  чертвертую лабу (крпитоситсема Эль-Гамаля)

--- FirstLabaCMK/Program.cs
+++ FirstLabaCMK/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Numerics;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace FirstLabaCMK
 {
@@ -8,13 +9,15 @@ namespace FirstLabaCMK
     {
         static void Main(string[] args)
         {
+            #region Laba123
+
             //ShowArray(SieveEratosthenes.GetPrimeNumbers(10));
             //SieveEratosthenes.ShowPrimeNumbers(1729);
 
-            foreach (var item in Converter.GetNumberFromDecimalToBinary(33))
-            {
-                Console.Write(item);
-            }
+            //foreach (var item in Converter.GetNumberFromDecimalToBinary(33))
+            //{
+            //    Console.Write(item);
+            //}
 
             //Console.WriteLine(BinaryPow.GetNumber(1920, 1021, 3551));
             //Console.WriteLine(BinaryPow.GetNumber(1805, 1021, 3551));
@@ -27,10 +30,49 @@ namespace FirstLabaCMK
             //MillerRabin.ShowHowInTask(n, 1, 3);
             //MillerRabin.ShowHowInTask(n, 1, 5);
             //MillerRabin.ShowHowInTask(n, 1, 7);
+            #endregion
+            #region Laba4
+
+            //Random rand = new Random();
+            //string alphabet = GetAlphaBet();
+            //Console.WriteLine(alphabet);
+
+            //int p = 23;
+            //int q = 5;//rand.Next(1, p);
+            //int c1, c2;
+            //// A пересылает(h) B
+            //int a = 6;//rand.Next(1, p);
+            //int h = BinaryPow.GetNumber(q, a, p);
+            //Console.WriteLine($"A пересылает(h = {h}) B");
+
+            //// B пересылает(r, c1) A
+            //int r = 10;// rand.Next(1, p);
+            //c1 = BinaryPow.GetNumber(q, r, p);
+            //Console.WriteLine($"B пересылает(r = {r}, c1 = {c1}) A");
+            //// A шифрует
+            //int M = 7;
+            //c2 = Cripto.GetModulo(M*BinaryPow.GetNumber(h, r, p), p);
+            //Console.WriteLine($"A шифрует M = {M} и пересылает(c1 = {c1}, c2 = {c2})");
+            //// B дешифрует
+            //int D;
+            ////D =
+            ////    Console.WriteLine($"B дешифрует");
+            #endregion
 
             Console.ReadKey();
 
         }
+
+        static string GetAlphaBet(int startIndexAscii = 65, int endIndexAscii = 90)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = startIndexAscii; i <= endIndexAscii; i++)
+            {
+                stringBuilder.Append((char)i);
+            }
+            return stringBuilder.ToString();
+        }
+
         static void ShowArray(int[] array)
         {
             Console.Write("[ ");
